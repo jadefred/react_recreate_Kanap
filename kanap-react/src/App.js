8d1c955700e8813eceaab7270c45f227a4cbbf5c
jadefred <97032726+jadefred@users.noreply.github.com>
2022-05-21 13:58:50 +0200

    OnePorduct.js function to add product to LS, validate input before submit, warning msg block pop when input is invalid

--- kanap-react/src/App.js
+++ kanap-react/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
@@ -7,14 +8,25 @@ import Products from "./components/productPage/Products";
 import OneProduct from "./components/productPage/OneProduct";
 import Footer from "./components/Footer";
 
+//add use status for the page - OneProduct, when it add to bag, change the stats of it parent - app
+//pass states as props
+
 function App() {
+  //state to save selected product - initial value is null or any products saved in LS
+  const [selectedProducts, setSelectProducts] = useState(JSON.parse(localStorage.getItem("products")));
+
+  //when children - OneProduct setState, update local storage accordingly
+  useEffect(() => {
+    localStorage.setItem("products", JSON.stringify(selectedProducts));
+  }, [selectedProducts]);
+
   return (
     <>
       <Router>
         <Header />
         <Routes>
           <Route path="/" element={<Products />} />
-          <Route path="/:id" element={<OneProduct />} />
+          <Route path="/:id" element={<OneProduct selectedProducts={selectedProducts} setSelectProducts={setSelectProducts} />} />
         </Routes>
       </Router>
       <Footer />
--- kanap-react/src/components/productPage/OneProduct.js
+++ kanap-react/src/components/productPage/OneProduct.js
@@ -2,11 +2,16 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "../css/oneProduct.css";
 
-function OneProduct() {
+function OneProduct(props) {
   const { id } = useParams();
   const [error, setError] = useState(null);
   const [item, setItem] = useState([]);
+  const [quantity, setQuantity] = useState("");
+  const [color, setColor] = useState("");
+  const [missingColor, setMissingColor] = useState(false);
+  const [missingQuantity, setMissingQuantity] = useState(false);
 
+  //fetch prouduct API by id
   useEffect(() => {
     fetch(`http://localhost:3000/api/products/${id}`)
       .then((res) => res.json())
@@ -19,6 +24,58 @@ function OneProduct() {
         }
       );
   }, []);
+
+  //pop warning message when the color / quantity is not correct, hide it when client modify them
+  useEffect(() => {
+    setMissingColor(false);
+  }, [color]);
+
+  useEffect(() => {
+    setMissingQuantity(false);
+  }, [quantity]);
+
+  function getQuantity(e) {
+    setQuantity(e.target.value);
+  }
+
+  function getColor(e) {
+    setColor(e.target.value);
+  }
+
+  //add to cart function
+  function addToCart(e) {
+    e.preventDefault();
+    //check if quantity and color is correct input
+    if (quantity > 0 && quantity <= 100 && color) {
+      //if in LS has no product, update props.setState directly in order to update LS
+      if (!props.selectedProducts) {
+        props.setSelectProducts([{ _id: id, quantity: quantity, color: color }]);
+      } //check if existing array in LS, if there any repeated product with same color
+      else if (props.selectedProducts.some((i) => i._id === id && i.color === color)) {
+        //create new array to handle change of state, then modify quantity accordingly, then update setState
+        let newArr = [];
+        for (const i of props.selectedProducts) {
+          if (i._id === id && i.color === color) {
+            let num = parseInt(i.quantity) + parseInt(quantity);
+            i.quantity = num.toString();
+          }
+          newArr.push(i);
+        }
+        props.setSelectProducts(newArr);
+      } //if no product is repeated, add new selected product to existing LS array
+      else {
+        props.setSelectProducts((prev) => [...prev, { _id: id, quantity: quantity, color: color }]);
+      }
+    } // if color or quantity is incorrect, pop warning message
+    else {
+      if (!color) {
+        setMissingColor(true);
+      }
+      if (quantity <= 0 || quantity > 100 || !quantity) {
+        setMissingQuantity(true);
+      }
+    }
+  }
 
   return (
     <>
@@ -41,7 +98,7 @@ function OneProduct() {
           <div id="one-product-color-quantity-box">
             <div id="color-selection">
               <label htmlFor="color-select">Choisir une couleur : </label>
-              <select name="color-select" id="colors">
+              <select name="color-select" id="colors" onChange={getColor}>
                 <option value="">--SVP, choisissez une couleur--</option>
                 {item.colors?.map((color) => {
                   return (
@@ -54,11 +111,24 @@ function OneProduct() {
             </div>
             <div id="quantity-selection">
               <label htmlFor="itemQuantity">Nombre d'article(s) (1-100) :</label>
-              <input type="number" name="itemQuantity" min="1" max="100" value="0" />
+              <input
+                type="number"
+                name="itemQuantity"
+                placeholder="0"
+                min="1"
+                max="100"
+                value={quantity}
+                onChange={getQuantity}
+              />
             </div>
           </div>
           <div id="add-to-bag-btn-box">
-            <button>Ajouter au panier</button>
+            <button onClick={addToCart}>Ajouter au panier</button>
+          </div>
+          {/* warning message appear only when input is incorrect and client want to submit */}
+          <div id="warning-msg-product">
+            {missingColor && <p>Choisissez une couleur</p>}
+            {missingQuantity && <p>La quantité est incorrecte</p>}
           </div>
         </div>
       )}
